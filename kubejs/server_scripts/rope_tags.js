28fb1a4fe10cc5783fd3abd4e9b172f5fba7f488
Zonbi <undead4life@gmail.com>
2026-03-25 20:59:33 -0400

    Rope Fix fix

--- kubejs/server_scripts/rope_tags.js
+++ kubejs/server_scripts/rope_tags.js
@@ -1,6 +1,6 @@
 // Adds supplementaries:rope to the farmersdelight:ropes tag.
 // This makes all FD recipes that consume rope via tag automatically
 // accept Supplementaries rope as an ingredient.
-StartupEvents.tags('item', event => {
+ServerEvents.tags('item', event => {
     event.add('farmersdelight:ropes', 'supplementaries:rope')
 })
